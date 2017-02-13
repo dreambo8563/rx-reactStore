fb1c533a2da827f238ab9df7e3be5750d80ac765
dreambo8563 <jie.guo@fenbeitong.com>
2017-02-13 23:06:56 +0800

    compare changes for selector

--- src/injectProps.js
+++ src/injectProps.js
@@ -1,33 +1,35 @@
-import React, { PropTypes } from 'react';
+import React, {PropTypes} from 'react';
+import deepEqual from 'deep-equal';
 
-const injectProps = (selector = (state) => state) => (WrappedComponent) =>
-    class injectProps extends React.Component {
+const injectProps = (selector = (state) => state) => (WrappedComponent) => class injectProps extends React.Component {
 
-      static contextTypes = {
-        store: PropTypes.object,
-      };
+  static contextTypes = {
+    store: PropTypes.object
+  };
 
-      constructor(props, context) {
-        super(props, context);
-        this.store = context.store
-      }
+  constructor(props, context) {
+    super(props, context);
+    this.store = context.store
+  }
 
-      componentWillMount() {
-        this.subscription = this.store
-        .map(selector)
-        .subscribe(::this.setState);
-      }
+  componentWillMount() {
+    this.subscription = this
+      .store
+      .map(selector)
+      .distinctUntilChanged((a, b) => deepEqual(a, b))
+      .subscribe(:: this.setState);
+  }
 
-      componentWillUnmount() {
-        this.subscription.unsubscribe();
-      }
+  componentWillUnmount() {
+    this
+      .subscription
+      .unsubscribe();
+  }
 
-      render() {
-          console.log(selector,WrappedComponent);
-        return (
-          <WrappedComponent { ...this.state } { ...this.props } />
-        );
-      }
-    };
+  render() {
+    console.log(selector, WrappedComponent);
+    return (<WrappedComponent { ...this.state } { ...this.props }/>);
+  }
+};
 
 export default injectProps;
